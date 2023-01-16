0af3195b672bbe8337c8db209935223dc6a4d847
Mikhail Ignatov <mikhail.ignatov@inbox.ru>
2023-01-17 00:55:56 +0300

    first commit

---
+++ Worker.java
@@ -0,0 +1,26 @@
+public class Worker implements OnTaskDoneListener, OnTaskErrorListener {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++)
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+    }
+
+    @Override
+    public void onDone(String result) {
+    }
+
+    @Override
+    public void onError(String errorResult) {
+    }
+}
